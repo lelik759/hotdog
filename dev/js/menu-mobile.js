17efea81b1d43fa4c184344541d93ed169dd44a6
lelik759 <llelik759@gmail.com>
2017-06-29 01:38:48 +0300

    адаптив
    осталось мобильное меню

---
+++ dev/js/menu-mobile.js
@@ -0,0 +1,19 @@
+
+
+var menuButton=document.querySelector(".header-nav__icon");
+var buttonClose=document.querySelector(".header-nav-hidden__icon");
+var headerNavHidden=document.querySelector(".header-nav-hidden");
+
+function openingMenu() { 
+	menuButton.style.opacity="0";
+	headerNavHidden.style.right="0";
+}
+
+function closingMenu() {
+	headerNavHidden.style.right="-100%";
+	headerNavHidden.style.transition="right 2s";
+	menuButton.style.opacity="0.8";
+}
+
+menuButton.addEventListener("click",openingMenu);
+buttonClose.addEventListener("click",closingMenu);
